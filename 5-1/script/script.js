fe0e6e5334fca44221d7014c4c71e36fb3445ebe
aso-ryo <2301181@s.asojuku.ac.jp>
2024-11-22 10:55:07 +0900

    G2更新

--- 5-1/script/script.js
+++ 5-1/script/script.js
@@ -2,7 +2,19 @@ new Vue({
     el: '#app',
     data() {
       return {
-        
+        menus:[{
+          label:'top',
+          path:'./index.html'
+        },{
+          label:'ABOUT',
+        psth:'./about.html'
+       },{
+        label:'SHEDULE',
+        path:'./schedule.html'
+       },{
+        label:'CONTACT',
+        path:'./contact.html'
+       }]
       };
     }
   });
